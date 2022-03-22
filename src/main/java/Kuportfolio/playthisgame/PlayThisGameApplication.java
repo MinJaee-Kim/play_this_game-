c37dfb858a4beec6c943ea5ac2911b6fa298cec6
MinJaee-Kim <75728238+MinJaee-Kim@users.noreply.github.com>
2022-03-22 14:52:33 +0900

    커밋

---
+++ src/main/java/Kuportfolio/playthisgame/PlayThisGameApplication.java
@@ -0,0 +1,13 @@
+package Kuportfolio.playthisgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayThisGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayThisGameApplication.class, args);
+	}
+
+}
---
+++ src/main/java/Kuportfolio/playthisgame/controller/HomeController.java
@@ -0,0 +1,25 @@
+package Kuportfolio.playthisgame.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/input")
+    public String input() {
+        return "input";
+    }
+
+    @PostMapping("/check")
+    public String check(Model model) {
+        model.addAttribute("name", "김민재");
+        return "check";
+    }
+}
