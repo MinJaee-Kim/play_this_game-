f2cb459d99e12e0fc3e2a25069e7df9a6b4adb3f
MinJaee-Kim <75728238+MinJaee-Kim@users.noreply.github.com>
2022-04-11 18:06:01 +0900

    체크박스 커밋

--- src/main/java/Kuportfolio/playthisgame/controller/InitThings.java
+++ src/main/java/Kuportfolio/playthisgame/controller/InitThings.java
@@ -15,9 +15,26 @@ public class InitThings {
         answerList.add(new Answer(7, "그렇다"));
         answerList.add(new Answer(10, "매우그렇다"));
 
+        questionList.add(new Question("casual", "질문 질문 질문 질문질문"));
+        questionList.add(new Question("story", "질문 질문 질문 질문질문2"));
+        questionList.add(new Question("challenge", "asd"));
+        questionList.add(new Question("collect", "asdf"));
+        questionList.add(new Question("tech", "asdff"));
+        questionList.add(new Question("challenge", "asd"));
+        questionList.add(new Question("collect", "asd"));
+        questionList.add(new Question("casual", "asd"));
+        questionList.add(new Question("collabo", "asd"));
+        questionList.add(new Question("story", "asdf"));
+        questionList.add(new Question("collabo", "asd"));
+        questionList.add(new Question("challenge", "asd"));
         questionList.add(new Question("casual", "asd"));
         questionList.add(new Question("story", "asdf"));
-        questionList.add(new Question("casd", "asdff"));
+        questionList.add(new Question("collect", "asd"));
+        questionList.add(new Question("tech", "asd"));
+        questionList.add(new Question("challenge", "asd"));
+        questionList.add(new Question("collabo", "asd"));
+        questionList.add(new Question("story", "asdf"));
+        questionList.add(new Question("tech", "asdff"));
 
         this.parameter = new String[][]{{"casual", "1"}, {"casual", "2"}};
     }
