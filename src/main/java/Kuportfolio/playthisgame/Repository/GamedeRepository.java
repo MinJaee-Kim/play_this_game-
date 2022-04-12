6c6f7cb9531c68cb767d52275436ca07cd086be3
qksksf5307 <84908629+qksksf5307@users.noreply.github.com>
2022-04-12 16:21:35 +0900

    ㅇㅇ
    ㅇㅇ

--- src/main/java/Kuportfolio/playthisgame/Repository/GamedeRepository.java
+++ src/main/java/Kuportfolio/playthisgame/Repository/GamedeRepository.java
@@ -9,11 +9,16 @@ import java.util.List;
 
 public interface GamedeRepository extends JpaRepository<Game,Long> {
     List<Game> findAll();
-    @Query(" from Game g where g.casual=:casual and g.collect=:collect and g.tech=:tech and g.collabo =:collabo and g.challenge = :challenge and g.story = :story")
-    List<Game> findScore(@Param("casual") Integer casual
-     ,@Param("collect") Integer collect
-            ,@Param("tech") Integer tech
-            ,@Param("collabo") Integer collabo
-            ,@Param("challenge") Integer challenge
-            ,@Param("story") Integer story);
+
+    @Query(value = " from Game g " +
+            "where g.casual=:casual and g.collect=:collect and g.tech=:tech and g.collabo =:collabo and g.challenge = :challenge and g.story = :story" +
+            "Between and g.casual-1 and g.collect+1 and ")
+    List<Game> findByCasualBetween(@Param("casual") Integer casual
+            , @Param("collect") Integer collect
+            , @Param("tech") Integer tech
+            , @Param("collabo") Integer collabo
+            , @Param("challenge") Integer challenge
+            , @Param("story") Integer story);
+
 }
+
--- src/main/java/Kuportfolio/playthisgame/Service/GamedeService.java
+++ src/main/java/Kuportfolio/playthisgame/Service/GamedeService.java
@@ -19,6 +19,6 @@ public class GamedeService {
     }
 
     public List<Game> getRecGame(GameDTO dto){
-        return repository.findScore(dto.getCasual(),dto.getCollect(),dto.getTech(),dto.getCollabo(),dto.getChallenge(), dto.getStory());
+        return repository.getOne(dto.getCasual(),dto.getCollect(),dto.getTech(),dto.getCollabo(),dto.getChallenge(), dto.getStory());
     }
 }
