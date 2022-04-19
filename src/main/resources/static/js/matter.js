543d12d699cd2698321b746fdde885eaa51a1128
MinJaee-Kim <75728238+MinJaee-Kim@users.noreply.github.com>
2022-04-19 12:57:22 +0900

    체크 문장

--- src/main/resources/static/js/matter.js
+++ src/main/resources/static/js/matter.js
@@ -3,7 +3,7 @@ function scrollT() {
 }
 
 
-function jbSubmit() {
+function inputSubmit() {
     let textBox = document.getElementById('myname');
     let form = document.getElementById('nameform');
     if (textBox.value.trim() == "") {
@@ -11,5 +11,27 @@ function jbSubmit() {
         return false
     }
     form.action = "/check";
+    form.submit();
+}
+
+function checkSubmit() {
+    let radioBox = document.querySelectorAll('.main-check-input');
+    let form = document.getElementById('checkform');
+    let count = 0;
+    for (var i=0; i<20; i++){
+        for (var j=0; j<5; j++){
+            if (radioBox[(i*5)+j].checked){
+                count++;
+                break;
+            }
+        }
+    }
+
+    if (count < 20){
+        alert("지문을 전부 체크해주세요 " + (20-count) + "개 남음");
+        return false;
+    }
+
+    form.action = "/result";
     form.submit();
 }
